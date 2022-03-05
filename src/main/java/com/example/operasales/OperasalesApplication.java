43235a41b7e2c4c2c68f352dc229f73b0cbffb08
avtor_house@mail.ru <Gtnheirfgi88>
2022-03-05 14:48:19 +0300

    Jdbc Repository

--- src/main/java/com/example/operasales/OperasalesApplication.java
+++ src/main/java/com/example/operasales/OperasalesApplication.java
@@ -24,31 +24,31 @@ public class OperasalesApplication {
         Opera opera2 = operaService.createOpera("Вишневый сад", "Пьеса по Чехову", 3);
         Opera opera3 = operaService.createOpera("Гамлет", "Пьеса по Шекспиру", 18);
 
-        // меняем оперу
-        opera.setName("Отелло");
-        operaService.createOpera(opera);
-        operaService.deleteOpera(opera3);
-
-        // вывод всех опер
-        System.out.println("Список премьер:");
-        operaService.getOperas().forEach(System.out::println);
-
-        // добавляем премьеру 1
-        LocalDateTime date = LocalDateTime.now();
-        OperaEvent operaEvent = operaEventService.createOperaEvent(date, opera, 25);
-        operaEventService.setBookEvent(operaEvent, 1);
-        operaEventService.setBookEvent(operaEvent, 11);
-        operaEventService.setBookEvent(operaEvent, 12);
-        operaEventService.cancelBookEvent(operaEvent, 12);
-
-        // добавляем премьеру 2
-        operaEventService.createOperaEvent(date, opera2, 25);
-
-        System.out.println("Афиша:");
-        operaEventService.getOperaEvents().forEach(System.out::println);
-
-        System.out.println("Кол-во свободных мест для премьеры " + operaEvent);
-        System.out.println(Arrays.toString(operaEvent.getSeats()));
+//        // меняем оперу
+//        opera.setName("Отелло");
+//        operaService.createOpera(opera);
+//        operaService.deleteOpera(opera3);
+//
+//        // вывод всех опер
+//        System.out.println("Список премьер:");
+//        operaService.getOperas().forEach(System.out::println);
+//
+//        // добавляем премьеру 1
+//        LocalDateTime date = LocalDateTime.now();
+//        OperaEvent operaEvent = operaEventService.createOperaEvent(date, opera, 25);
+//        operaEventService.setBookEvent(operaEvent, 1);
+//        operaEventService.setBookEvent(operaEvent, 11);
+//        operaEventService.setBookEvent(operaEvent, 12);
+//        operaEventService.cancelBookEvent(operaEvent, 12);
+//
+//        // добавляем премьеру 2
+//        operaEventService.createOperaEvent(date, opera2, 25);
+//
+//        System.out.println("Афиша:");
+//        operaEventService.getOperaEvents().forEach(System.out::println);
+//
+//        System.out.println("Кол-во свободных мест для премьеры " + operaEvent);
+//        System.out.println(Arrays.toString(operaEvent.getSeats()));
     }
 
 }
--- src/main/java/com/example/operasales/domain/Opera.java
+++ src/main/java/com/example/operasales/domain/Opera.java
@@ -2,12 +2,15 @@ package com.example.operasales.domain;
 
 import com.sun.istack.NotNull;
 import lombok.Data;
+import org.springframework.context.annotation.Primary;
 
+import javax.persistence.Id;
 import java.util.UUID;
 
 @Data
 public class Opera {
     @NotNull
+    @Id
     private String id;
     private  String name;
     private  String desc;
